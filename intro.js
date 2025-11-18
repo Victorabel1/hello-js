b8044092265dcfa79d20c2fda3a77fddf42caf57
Victor <victordamina00@gmail.com>
2025-11-18 13:15:51 +0100

    updated

--- intro.js
+++
@@ -1,3 +0,0 @@
-console.log("Name: Victor Abel");
-console.log("Age: 33");
-console.log("Favorite Programming Language: JavaScript");
---
+++ week 1/intro.js
@@ -0,0 +1,4 @@
+console.log("Name: Victor Abel");
+console.log("Age: 33");
+console.log("Favorite Programming Language: JavaScript");
+
